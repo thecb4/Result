0bc0acc99e79e4bf22a98069247aba426202ba3b
TheCB4 <cavelle@thecb4.io>
2018-02-25 07:43:09 -0500

    phoneOS,tvOS,watchOS

---
+++ Result-watchOS/Result_watchOS.h
@@ -0,0 +1,18 @@
+//
+//  Result_watchOS.h
+//  Result-watchOS
+//
+//  Created by Cavelle Benjamin on 18-Feb-25 (08).
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Result_watchOS.
+FOUNDATION_EXPORT double Result_watchOSVersionNumber;
+
+//! Project version string for Result_watchOS.
+FOUNDATION_EXPORT const unsigned char Result_watchOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Result_watchOS/PublicHeader.h>
+
+
