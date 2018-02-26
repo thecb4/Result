54933fd582f96e1b4d648d3c25b7e10a98f1a806
TheCB4 <cavelle@thecb4.io>
2018-02-25 21:03:37 -0500

    fixed Supported Platforms in Schemes for Carthage

---
+++ Result-macOS/Result_macOS.h
@@ -0,0 +1,18 @@
+//
+//  Result_macOS.h
+//  Result-macOS
+//
+//  Created by Cavelle Benjamin on 18-Feb-25 (08).
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for Result_macOS.
+FOUNDATION_EXPORT double Result_macOSVersionNumber;
+
+//! Project version string for Result_macOS.
+FOUNDATION_EXPORT const unsigned char Result_macOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Result_macOS/PublicHeader.h>
+
+
