6129b6a569da5d74be53c9110bdf967ab164f609
TheCB4 <cavelle@thecb4.io>
2018-04-01 00:42:31 -0400

    added methods to protocol

--- Sources/Result/Core/ResultProtocol.swift
+++ Sources/Result/Core/ResultProtocol.swift
@@ -17,4 +17,20 @@ public protocol ResultProtocol {
   init(error: Error)
   
   var result: Result<Value, Error> { get }
+  
+  /// Case analysis for ResultProtocol.
+  ///
+  /// Returns the value produced by appliying `ifFailure` to the error if self represents a failure, or `ifSuccess` to the result value if self represents a success.
+  func analysis<U>(ifSuccess: (Value) -> U, ifFailure: (Error) -> U) -> U
+  
+  /// Returns the value if self represents a success, `nil` otherwise.
+  ///
+  /// A default implementation is provided by a protocol extension. Conforming types may specialize it.
+  var value: Value? { get }
+  
+  /// Returns the error if self represents a failure, `nil` otherwise.
+  ///
+  /// A default implementation is provided by a protocol extension. Conforming types may specialize it.
+  var error: Error? { get }
+  
 }
